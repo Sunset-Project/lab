7bab3f007db50c1ed3ac2304e71be929f8be1284
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-06-18 16:32:25 -0700

    Assert panic is now tested

--- test/automated/assert_test.go
+++ test/automated/assert_test.go
@@ -12,11 +12,10 @@ import (
 func TestAssert(t *testing.T) {
 	Context, Test, Assert := lab.StartSession(t)
 	Context("Assert", func() {
-		session, reporter, controller := controls.SessionExample()
+		session, reporter, _ := controls.SessionExample()
+		assert := session.Assertion()
 
 		Context("True", func() {
-			assert := session.Assertion()
-
 			assert(true)
 			signals := reporter.LastRecordedSignals(1)
 
@@ -26,9 +25,6 @@ func TestAssert(t *testing.T) {
 		})
 
 		Context("False", func() {
-			assert := session.Assertion()
-			immediateFailures := controller.ImmediateFailures
-
 			session.Test("", func() { panic(nil) })
 
 			Test("Panics with AssertionError", func() {
@@ -41,14 +37,30 @@ func TestAssert(t *testing.T) {
 				)
 			})
 
-			Test("Test controller Fails Now", func() {
-				Assert(controller.ImmediateFailures == immediateFailures+1)
-			})
-
 			signals := reporter.LastRecordedSignals(1)
 
 			Test("Report sequence is Asserted", func() {
 				Assert(signals[0] == reporting.SigAsserted)
+			})
+		})
+
+		Context("Panic", func() {
+			Context("Function with no panic", func() {
+				Test("Panics with AssertionError", func() {
+					Assert.PanicMsg(
+						func() { assert.Panic(func() {}) },
+						func(err interface{}) bool {
+							_, ok := err.(asserting.AssertionError)
+							return ok
+						},
+					)
+				})
+
+				signals := reporter.LastRecordedSignals(1)
+
+				Test("Report sequence is Asserted", func() {
+					Assert(signals[0] == reporting.SigAsserted)
+				})
 			})
 		})
 	})
